7a3111acc62c4dd8a265ab5a873d4ad23024a645
minhhoanq <mh.tranminhhoang@gmail.com>
2024-07-05 10:34:51 +0700

    server review checkout

--- server/src/application/usecases/checkout/checkout.service.ts
+++ server/src/application/usecases/checkout/checkout.service.ts
@@ -28,24 +28,53 @@ export class CheckoutService implements ICheckoutService {
         this._cartItemRepo = cartItemRepo;
     }
 
+    //checkout review all product items for user
     async checkout(userId: number, payload: any): Promise<any> {
-        console.log(userId);
-
         const cart = await this._cartRepo.findByUserId(userId);
-        console.log(cart);
 
         if (!cart) throw new NotFoundError("Cart not found!");
-        // const listProducts = await Promise.all(
-        //     payload.listItems.map(
-        //         async (item: any) =>
-        //             await this._productItemRepo.findById(item.productItemId)
-        //     )
-        // );
 
-        // console.log(listProducts);
         const cartItems = await this._cartItemRepo.findByUserId(userId);
-        console.log(cartItems);
 
-        return cartItems;
+        const checkoutOrder = {
+            total: 0,
+            feeShip: 0,
+            totalDiscount: 0,
+            totalCheckout: 0,
+        };
+
+        //check product is exists
+        const listProducts = await Promise.all(
+            payload.listItems.map(
+                async (item: any) =>
+                    await this._productItemRepo.findById(item.productItemId)
+            )
+        );
+
+        //match listitems of user with cartitems of user
+        const commonItems = cartItems.filter((cartItem: any) =>
+            listProducts.some(
+                (item: any) => item.id === cartItem.id
+                // item.quantity === cartItem.quantity
+            )
+        );
+
+        //review total price
+        const total = commonItems.reduce(
+            (acc: number, item: any) => acc + item.salePrice * item.quantity,
+            0
+        );
+
+        //handle checkout, feeship
+        //handle checkout, feeship
+
+        checkoutOrder.total = total;
+        // review again total, but not discount
+        checkoutOrder.totalCheckout = total;
+
+        return {
+            orderItems: commonItems,
+            checkoutOrder: checkoutOrder,
+        };
     }
 }
