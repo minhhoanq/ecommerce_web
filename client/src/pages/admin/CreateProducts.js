ad51d068f216a45ac43210de695fd877132eed9c
minhhoanq <mh.tranminhhoang@gmail.com>
2024-07-29 16:44:24 +0700

    create product

--- client/src/pages/admin/CreateProducts.js
+++ client/src/pages/admin/CreateProducts.js
@@ -6,6 +6,7 @@ import { validate, getBase64 } from "ultils/helpers";
 import { toast } from "react-toastify";
 import { apiCreateProduct } from "apis";
 import { showModal } from "store/app/appSlice";
+import { IoImagesOutline } from "react-icons/io5";
 
 const CreateProducts = () => {
     const { categories } = useSelector((state) => state.app);
@@ -57,36 +58,69 @@ const CreateProducts = () => {
     }, [watch("images")]);
 
     const handleCreateProduct = async (data) => {
-        const invalids = validate(payload, setInvalidFields);
-        if (invalids === 0) {
-            if (data.category)
-                data.category = categories?.find(
-                    (el) => el._id === data.category
-                )?.title;
-            const finalPayload = { ...data, ...payload };
-            const formData = new FormData();
-            for (let i of Object.entries(finalPayload))
-                formData.append(i[0], i[1]);
-            if (finalPayload.thumb)
-                formData.append("thumb", finalPayload.thumb[0]);
-            if (finalPayload.images) {
-                for (let image of finalPayload.images)
-                    formData.append("images", image);
-            }
-            dispatch(
-                showModal({ isShowModal: true, modalChildren: <Loading /> })
-            );
-            const response = await apiCreateProduct(formData);
-            dispatch(showModal({ isShowModal: false, modalChildren: null }));
-            if (response.success) {
-                toast.success(response.mes);
-                reset();
-                setPayload({
-                    thumb: "",
-                    image: [],
-                });
-            } else toast.error(response.mes);
-        }
+        console.log("create data product: ", data);
+        const payload = {
+            name: data.title,
+            desc: "data.description",
+            // categoryId: data.category,
+            // brandId: data.brand,
+            categoryBrandId: 2,
+            image: "https://cdn2.cellphones.com.vn/358x/media/catalog/product/p/h/photo_2022-09-28_21-58-51_5.jpg",
+            images: [
+                "https://cdn2.cellphones.com.vn/insecure/rs:fill:0:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/p/h/photo_2022-09-28_21-58-54_5.jpg",
+                "https://cdn2.cellphones.com.vn/insecure/rs:fill:0:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/p/h/photo_2022-09-28_21-58-57_6.jpg",
+                "https://cdn2.cellphones.com.vn/insecure/rs:fill:0:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/p/h/photo_2022-09-28_21-58-48_5.jpg",
+            ],
+            skus: [
+                {
+                    name: data.name,
+                    price: data.price,
+                    stock: data.quantity,
+                    attributes: [
+                        {
+                            attributeId: 1,
+                            attributeValue: data.color,
+                        },
+                        {
+                            attributeId: 2,
+                            attributeValue: data.storage,
+                        },
+                    ],
+                },
+            ],
+        };
+
+        console.log(payload);
+        // const invalids = validate(payload, setInvalidFields);
+        // if (invalids === 0) {
+        //     if (data.category)
+        //         data.category = categories?.find(
+        //             (el) => el._id === data.category
+        //         )?.title;
+        //     const finalPayload = { ...data, ...payload };
+        //     const formData = new FormData();
+        //     for (let i of Object.entries(finalPayload))
+        //         formData.append(i[0], i[1]);
+        //     if (finalPayload.thumb)
+        //         formData.append("thumb", finalPayload.thumb[0]);
+        //     if (finalPayload.images) {
+        //         for (let image of finalPayload.images)
+        //             formData.append("images", image);
+        //     }
+        //     dispatch(
+        //         showModal({ isShowModal: true, modalChildren: <Loading /> })
+        //     );
+        //     const response = await apiCreateProduct(formData);
+        //     dispatch(showModal({ isShowModal: false, modalChildren: null }));
+        //     if (response.success) {
+        //         toast.success(response.mes);
+        //         reset();
+        //         setPayload({
+        //             thumb: "",
+        //             image: [],
+        //         });
+        //     } else toast.error(response.mes);
+        // }
     };
     return (
         <div className="w-full">
@@ -110,8 +144,8 @@ const CreateProducts = () => {
                         <Select
                             label="Category"
                             options={categories?.map((el) => ({
-                                code: el._id,
-                                value: el.title,
+                                code: el.id,
+                                value: el.name,
                             }))}
                             register={register}
                             id="category"
@@ -123,8 +157,11 @@ const CreateProducts = () => {
                         <Select
                             label="Brand"
                             options={categories
-                                ?.find((el) => el._id === watch("category"))
-                                ?.brand?.map((el) => ({ code: el, value: el }))}
+                                ?.find((el) => el.id == watch("category"))
+                                ?.brands?.map((el) => ({
+                                    code: el.id,
+                                    value: el.name,
+                                }))}
                             register={register}
                             id="brand"
                             style="flex-auto"
@@ -141,64 +178,97 @@ const CreateProducts = () => {
                         invalidFields={invalidFields}
                         setInvalidFields={setInvalidFields}
                     />
-                    <div className="flex flex-col gap-2 mt-8">
-                        <label className="font-semibold" htmlFor="thumb">
-                            Upload thumb
-                        </label>
-                        <input
-                            type="file"
-                            id="thumb"
-                            {...register("thumb", { required: "Need fill" })}
-                        />
-                        {errors["thumb"] && (
-                            <small className="text-xs text-red-500">
-                                {errors["thumb"]?.message}
-                            </small>
-                        )}
-                    </div>
-                    {preview.thumb && (
-                        <div className="my-4">
-                            <img
-                                src={preview.thumb}
-                                alt="thumbnail"
-                                className="w-[200px] object-contain"
+                    <div className="flex space-x-4">
+                        <div className="flex flex-col gap-2 mt-2">
+                            <label
+                                className="font-semibold flex flex-col space-y-2"
+                                htmlFor="thumb"
+                            >
+                                <span>Upload representative image</span>
+                                {preview.thumb ? (
+                                    <div className="my-4">
+                                        <img
+                                            src={preview.thumb}
+                                            alt="thumbnail"
+                                            className="w-[150px] h-[150px] object-cover"
+                                        />
+                                    </div>
+                                ) : (
+                                    <div className="h-[100px] w-[100px] border-2 border-dashed border-gray-400 flex justify-center items-center">
+                                        <IoImagesOutline
+                                            size={50}
+                                            color="#9ca3af"
+                                        />
+                                    </div>
+                                )}
+                            </label>
+                            <input
+                                type="file"
+                                id="thumb"
+                                {...register("thumb", {
+                                    required: "Need fill",
+                                })}
+                                hidden
                             />
+                            {errors["thumb"] && (
+                                <small className="text-xs text-red-500">
+                                    {errors["thumb"]?.message}
+                                </small>
+                            )}
                         </div>
-                    )}
-                    <div className="flex flex-col gap-2 mt-8">
-                        <label className="font-semibold" htmlFor="products">
-                            Upload images of product
-                        </label>
-                        <input
-                            type="file"
-                            id="products"
-                            multiple
-                            {...register("images", { required: "Need fill" })}
-                        />
-                        {errors["images"] && (
-                            <small className="text-xs text-red-500">
-                                {errors["images"]?.message}
-                            </small>
-                        )}
+
+                        <div className="flex flex-col gap-2 mt-2">
+                            <label
+                                className="font-semibold  flex flex-col space-y-2"
+                                htmlFor="products"
+                            >
+                                <span>Upload images</span>
+                                {preview.images.length > 0 ? (
+                                    <div className="my-4 flex w-full gap-3 flex-wrap">
+                                        {preview.images?.map((el, idx) => (
+                                            <div
+                                                key={idx}
+                                                className="w-fit relative"
+                                            >
+                                                <img
+                                                    src={el.path}
+                                                    alt="product"
+                                                    className="w-[150px] h-[150px] object-cover"
+                                                />
+                                            </div>
+                                        ))}
+                                    </div>
+                                ) : (
+                                    <div className="h-[100px] w-[100px] border-2 border-dashed border-gray-400 flex justify-center items-center">
+                                        <IoImagesOutline
+                                            size={50}
+                                            color="#9ca3af"
+                                        />
+                                    </div>
+                                )}
+                            </label>
+                            <input
+                                type="file"
+                                id="products"
+                                multiple
+                                {...register("images", {
+                                    required: "Need fill",
+                                })}
+                                hidden
+                            />
+                            {errors["images"] && (
+                                <small className="text-xs text-red-500">
+                                    {errors["images"]?.message}
+                                </small>
+                            )}
+                        </div>
                     </div>
-                    {preview.images.length > 0 && (
-                        <div className="my-4 flex w-full gap-3 flex-wrap">
-                            {preview.images?.map((el, idx) => (
-                                <div key={idx} className="w-fit relative">
-                                    <img
-                                        src={el.path}
-                                        alt="product"
-                                        className="w-[200px] object-contain"
-                                    />
-                                </div>
-                            ))}
-                        </div>
-                    )}
+
                     <InputForm
                         label="Product name with attributes (Ex: Macbook 256GB)"
                         register={register}
                         errors={errors}
-                        id="title"
+                        id="name"
                         validate={{
                             required: "Need fill this field",
                         }}
@@ -206,9 +276,7 @@ const CreateProducts = () => {
                         placeholder="Name of new product"
                     />
                     <div className="w-full my-6 flex gap-4">
-                        <div
-                            className={"flex flex-col w-[800px] h-[78px] gap-2"}
-                        >
+                        <div className={"flex flex-col w-[600px] gap-2"}>
                             <div className="flex items-center space-x-2">
                                 <label className="font-medium">
                                     {"Attributes" + ":"}
@@ -244,9 +312,32 @@ const CreateProducts = () => {
                                     </div>
                                 </div>
                             </div>
-                            <div>aaaa</div>
+                            <div className="flex flex-col space-y-4">
+                                <InputForm
+                                    label="Stogare"
+                                    register={register}
+                                    errors={errors}
+                                    id="storage"
+                                    validate={{
+                                        required: "Need fill this field",
+                                    }}
+                                    style="flex-auto"
+                                    placeholder="Storage of new product"
+                                />
+                                <InputForm
+                                    label="Color"
+                                    register={register}
+                                    errors={errors}
+                                    id="color"
+                                    validate={{
+                                        required: "Need fill this field",
+                                    }}
+                                    style="flex-auto"
+                                    placeholder="Color of new product"
+                                />
+                            </div>
                         </div>
-                        <div className="flex flex-col flex-1">
+                        <div className="flex flex-col flex-1 space-y-4 h-[78px]">
                             <InputForm
                                 label="Price"
                                 register={register}
@@ -272,17 +363,6 @@ const CreateProducts = () => {
                                 type="number"
                             />
                         </div>
-                        {/* <InputForm
-                            label="Color"
-                            register={register}
-                            errors={errors}
-                            id="color"
-                            validate={{
-                                required: "Need fill this field",
-                            }}
-                            style="flex-auto"
-                            placeholder="color of new product"
-                        /> */}
                     </div>
                     <div className="my-6">
                         <Button type="submit">Create new product</Button>
